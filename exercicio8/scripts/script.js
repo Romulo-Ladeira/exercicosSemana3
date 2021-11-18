15f5b7d369fe550cd175d1555d1ceee97c294a74
romuloLadeira <romulo.alesandro@gmail.com>
2021-11-18 16:08:31 -0300

    adiciona-exercicios-8-e-9

---
+++ exercicio8/scripts/script.js
@@ -0,0 +1,19 @@
+const tarefa = document.getElementById("novo-item")
+const btnAdd = document.getElementById("bntAdd")
+const itensLista = document.getElementById("itens-lista")
+const lista = document.getElementById("lista")
+
+btnAdd.addEventListener('click',event => {
+    if(tarefa.value === ""){
+        window.alert("Por favor adicione algum texto")
+    }else{
+        let item = document.createElement('option')
+        item.innerText = tarefa.value
+
+        itensLista.appendChild(item)
+    }
+    
+    localStorage.setItem('lista', lista.innerHTML)
+    
+    tarefa.value= ''
+})
